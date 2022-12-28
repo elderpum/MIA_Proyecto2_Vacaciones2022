ba804f88739cbb9f6b56cb4836f9a5fe1178b8bc
Elder Pum <ElderPum@gmail.com>
2022-12-28 07:53:16 -0600

    Bugfixes relacionados con el funcionamiento del s3 bucket. Falta cognito

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const fs = require('fs')
 const path = require('path')
 const { response, request } = require('express')
+const {uploadFile} = require('../helpers/awss3')
 
 const createUser = async (req = request, res = response) => {
     const file = fs.readFileSync(path.resolve(__dirname, "../database/users.json"), 'utf-8');
     let jsonUsers = JSON.parse(file)
+    const puerto = process.env.PORT
+    console.log(puerto)
     
     // Get text params in request body
     const {id, name, username, email, password, confirmPass, tipoUser} = req.body
@@ -23,21 +27,25 @@ const createUser = async (req = request, res = response) => {
     const extension = picture.name.substring(picture.name.lastIndexOf('.'))
     const nombreArchivo = `${username.replace(/[^\w\s]/gi, '')}/profile${extension}`
 
+
     // We need to open users.json and insert data in
     try {
+        const {url: urlPerfil, nombre} = await uploadFile(nombreArchivo, picture.data)
         var newUser = {
             id: id,
             name: name,
             username: username,
-            profilePhoto: nombreArchivo,
+            profilePhoto: urlPerfil,
             email: email,
             password: password,
             confirmPass: confirmPass,
             tipoUser: tipoUser
         }
 
+        // Now we gotta push new user into json file
         jsonUsers.users.push(newUser)
 
+        // Write new user in json file without losing any other data
         const new_json_users = JSON.stringify(jsonUsers)
         fs.writeFileSync(path.resolve(__dirname, "../database/users.json"), new_json_users, 'utf-8')
 
---
+++ backend/helpers/awss3.js
@@ -0,0 +1,57 @@
+const AWS = require('aws-sdk');
+
+const credentials = {
+    ID: process.env.AWS_ACCESS_KEY_ID,
+    SECRET: process.env.AWS_SECRET_ACCESS_KEY,
+    BUCKET_NAME: process.env.AWS_S3_NAME
+}
+
+const s3 = new AWS.S3({
+    accessKeyId: credentials.ID,
+    secretAccessKey: credentials.SECRET
+})
+
+
+const uploadFile = (filename, fileData) => {
+    const name = filename;
+    const params = {
+        Bucket: credentials.BUCKET_NAME,
+        Key: name,
+        Body: fileData
+    }
+    return new Promise((resolve, reject) => {
+        s3.upload(params, (err, data) => {
+            if (err) {
+                console.log(err)
+                reject(err)
+            }
+            console.log(s3)
+            console.log(data)
+            resolve({ url: data.Location, nombre: name })
+        })
+    })
+
+}
+
+
+const deleteFile = (fileName) => {
+    const params = {
+        Bucket: credentials.BUCKET_NAME,
+        Key: fileName,
+    }
+    return new Promise((resolve, reject) => {
+    
+        s3.deleteObject(params, (err) => {
+            if (err) {
+                reject(err)
+            }
+            resolve({ msg : 'Eliminador con exito' });
+        })
+
+    })
+}
+
+module.exports = {
+    uploadFile,
+    deleteFile
+}
