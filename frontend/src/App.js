ca23ff3c397bde4621ac97f9762fbe1a5c1aef3f
Elder Pum <ElderPum@gmail.com>
2023-01-01 02:05:23 -0600

    Login funcional, falta redirigir a página main y al registro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,29 +3,50 @@ import logo from "./assets/logo.png";
 import "./App.css";
 
 class App extends Component {
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log(e.target.email.value);
 
-    if (!e.target.email.value) {
-      alert("Email is required");
-    } else if (!e.target.email.value) {
-      alert("Valid email is required");
-    } else if (!e.target.password.value) {
-      alert("Password is required");
-    } else if (
-      e.target.email.value === "me@example.com" &&
-      e.target.password.value === "123456"
-    ) {
-      alert("Successfully logged in");
-      e.target.email.value = "";
-      e.target.password.value = "";
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.username.value);
+
+    if (!e.target.username.value && !e.target.password.value) {
+      alert("Llene el campo de nombre de usuario y contraseña.");
+    } else if (!e.target.username.value || !e.target.password.value) {
+      alert(
+        "Uno de los campos está vacío. Por favor llenarlo antes de iniciar sesión."
+      );
     } else {
-      alert("Wrong email or password combination");
+      const data = {
+        username: e.target.username.value,
+        password: e.target.password.value,
+      };
+
+      // Obtenemos la ruta de la api correspondiente al login
+      fetch("http://localhost:3200/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }) .then(async response => {
+        const isJson = response.headers.get('content-type')?.includes('application/json')
+        const Data = isJson && await response.json()
+
+        if (!response.ok) {
+          const error = (data && data.message) || response.status
+          return Promise.reject(error)
+        }
+
+        // Here we need to move to another react page
+        alert("awelas si se loguea y reconoce el usuario y la contraseña")
+      }) .catch(error => {
+        alert("Hubo un error al iniciar sesión. Vuelva a intentarlo")
+        console.log(error)
+      });
+      
     }
   };
 
-  handleClick = e => {
+  handleClick = (e) => {
     e.preventDefault();
 
     alert("Goes to registration page");
@@ -37,8 +58,12 @@ class App extends Component {
         <img src={logo} className="logo" alt="Business view - Reports" />
         <form className="form" onSubmit={this.handleSubmit}>
           <div className="input-group">
-            <label htmlFor="email">Nombre de Usuario</label>
-            <input type="email" name="email" placeholder="example@gmail.com" />
+            <label htmlFor="username">Nombre de Usuario</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Nombre de Usuario"
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Contraseña</label>
